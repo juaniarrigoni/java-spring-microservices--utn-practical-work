7555868b85df451af01a3fec4027e03cbfdb4788
Juan Ignacio (Juani) Arrigoni <101672354+juaniarrigoni@users.noreply.github.com>
2025-11-06 21:56:07 -0300

    Align gateway routing and dockerized configs

--- api-gateway/src/main/java/com/contenedores/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/contenedores/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,14 @@ package com.contenedores.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.contenedores.apigateway.config.GatewayProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GatewayProperties.class)
 public class ApiGatewayApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(ApiGatewayApplication.class, args);
+        }
 }
